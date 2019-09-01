214b6d1ec3bdb95a208c1c9a0e4e16e403488f9f
Schartz Rehan <rehan.qt@gmail.com>
2019-09-01 17:29:19 +0530

    Add preliminary login for making web requests. Nothing to see here folks

---
+++ models/app_state.py
@@ -0,0 +1,23 @@
+class AppState:
+    def __init__(self):
+        self.initialized = 0
+        self.completed = 0
+        self.request_attributes = RequestAttributes()
+        self.response_attributes = ResponseAttributes()
+
+
+class RequestAttributes:
+    def __init__(self):
+        self.request_type = 'get'
+        self.url = 'url'
+        self.parameters = {}
+        self.authorization = {'type': 'noauth', 'token': ''}
+        self.headers = {}
+        self.body = ''
+
+
+class ResponseAttributes:
+    def __init__(self):
+        self.status_code = 0
+        self.raw_response = ''
+        self.headers = {}
--- rester.py
+++ rester.py
@@ -1,19 +1,59 @@
-from PyQt5 import QtWidgets, uic
-import sys
+from PyQt5 import QtWidgets, QtGui
+import sys, json
+import urllib3
+import models.app_state as app_state
+from ui.ui import Ui_mainWindow as ResterUiWindow
 
 
-class RestApp(QtWidgets.QMainWindow):
+class Rester(ResterUiWindow):
     def __init__(self):
-        super(RestApp, self).__init__()
-        uic.loadUi('rester.ui', self)
-        self.show()
+        super().__init__()
+        self.app_state = app_state.AppState()
+        self.http = urllib3.PoolManager()
+
+    def on_url_type_changed(self, text):
+        self.app_state.request_attributes.request_type = text
+
+    def handle_request_send(self):
+        self.app_state.request_attributes.url = self.url.text()
+        response = self.http.request('GET', self.app_state.request_attributes.url)
+        raw_response = str(response.data)
+        headers = response.headers
+
+        self.response_raw.setText(raw_response)
+
+        self.app_state.response_attributes.raw_response = raw_response
+        self.app_state.response_attributes.headers = headers
+
+        self.response_headers_table.setRowCount(len(headers))
+        i = 0
+        for key in headers:
+            self.response_headers_table.setItem(i, 0, QtWidgets.QTableWidgetItem(key))
+            self.response_headers_table.setItem(i, 1, QtWidgets.QTableWidgetItem(headers[key]))
+            i += 1
+
+
+def bind_actions(rester: Rester):
+    # connect URL type combo box with internal variable
+    rester.request_type.activated[str].connect(rester.on_url_type_changed)
+    rester.send_btn.clicked.connect(rester.handle_request_send)
 
 
 # Create an instance of QtWidgets.QApplication
 app = QtWidgets.QApplication(sys.argv)
 
-# Create an instance of our class
-window = RestApp()
+# Create an instance of QtWidgets.QMainWindow
+mainWindow = QtWidgets.QMainWindow()
 
-# Start the application
-app.exec_()
+# Create an instance of our application class
+rester_main_window = Rester()
+
+# Bind Qt rendering of QtWidgets.QMainWindow with our main window
+rester_main_window.setupUi(mainWindow)
+bind_actions(rester_main_window)
+
+# show it!
+mainWindow.show()
+
+# Without this line, the window of Rester will not show up
+sys.exit(app.exec_())
